0863c265486662a22c060f244e4d4a097366fc4e
Niklas Aichinger <aichinger.niklas@gmail.com>
2023-04-11 13:12:07 +0200

    k8s

--- demoapp/src/main/java/org/acme/CarService.java
+++ demoapp/src/main/java/org/acme/CarService.java
@@ -5,9 +5,8 @@ import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Path("cars")
+@Path("")
 public class CarService {
-    @Path("all")
     @GET
     public List<Car> findAll() {
         return Car.findAll().list();
